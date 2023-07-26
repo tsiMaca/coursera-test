700bd5274ccdb13160c796dd7543190382addb32
Macarena Santos <macarena.santos@awsys.com.ar>
2023-07-26 14:15:20 -0300

    DI Lecture 09

---
+++ Lecture09/app.js
@@ -0,0 +1,14 @@
+(function(){
+    'use strict';
+    angular.module('DIApp',[])
+    .controller('DIController', DIController);
+
+    function DIController ($scope, $filter){
+        $scope.name ="Maca";
+
+        $scope.upper = function(){
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name)
+        }
+    }
+})()
